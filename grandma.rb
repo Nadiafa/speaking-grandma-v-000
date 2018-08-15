01790aa1a8347cbf7af3d39a414e959866cccee0
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-15 09:22:28 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase = phrase.upcase ? "NO, NOT SINCE 1938!" : 
+  if phrase = phrase.upcase
+    puts "NO, NOT SINCE 1938!"
+  elsif phrase = ""
+  "HUH?! SPEAK UP, SONNY!"
 end 
 
 # Whatever you say to grandma, she should respond with
