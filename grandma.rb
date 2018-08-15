b2bcee866bcce006b2092e2a49b813f3507e55c4
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-15 09:30:57 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,8 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase && phrase == "I LOVE YOU GRANDMA!"
       "I LOVE YOU TOO PUMPKIN!"
-
+  elsif phrase == phrase.upcase
+  
   # if phrase == phrase.upcase
   #   puts "NO, NOT SINCE 1938!"
   # elsif phrase == "I LOVE YOU GRANDMA!"
